5b9cc1fe4206435369fe138fe4ec2db49e3fbff8
OmarDIOP0 <odiop6170@gmail.com>
2024-12-16 16:51:27 +0000

    ajout_de_quelque_page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { HeaderComponent } from './shared/layouts/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FooterComponent],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
