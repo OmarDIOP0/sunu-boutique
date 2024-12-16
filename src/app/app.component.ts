8f4ec4581389ffeb977b83bb5b02cfbf00b19d1f
OmarDIOP0 <odiop6170@gmail.com>
2024-12-16 19:22:26 +0000

    migration_sur_tailwind_css_category_section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
 import { HeaderComponent } from './shared/layouts/header/header.component';
+import { CategoryComponent } from './shared/layouts/category/category.component';
+import { PromoComponent } from './shared/layouts/promo/promo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FooterComponent,HeaderComponent],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,CategoryComponent,PromoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
