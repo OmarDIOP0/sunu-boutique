4969081485d9cfb80753fa8bfab1f34a5177befb
OmarDIOP0 <odiop6170@gmail.com>
2024-12-16 13:59:11 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'e-commerce';
